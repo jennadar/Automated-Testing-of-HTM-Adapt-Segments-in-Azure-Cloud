a18c021ed5b3b4884db47d880d6eb94de5474e6f
Damir Dobric <ddobric@daenet.de>
2022-10-31 18:05:01 +0100

    Adde my first app

---
+++ Source/myfirstapp/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello best students in the world! :)");
+
+int a = 71;
+
+int b = 1;
+
+int c = a + b *2123;
+
+c = MyFunction.AddTwoNumbers(2837387, 2332);
+
+Console.WriteLine($"The result is: {c}");
+
+Console.WriteLine("Please enter something smart :)");
+
+var userInput = Console.ReadLine();
+
+Console.WriteLine($"You have entered something not smart: ha, ha {userInput}");
+
+Console.WriteLine("Please hit any key to exit...)");
+
+Console.ReadLine();
