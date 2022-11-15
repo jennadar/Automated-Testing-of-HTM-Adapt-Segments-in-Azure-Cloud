24744d3d40b1814c50baed972a7c2aa604814af8
Theertha <theertha1808@gmail.com>
2022-11-15 05:22:12 +0100

    Project created for Ex4 with Library dependency

---
+++ MyWork/Ex4/HelloWorldWithClass/HelloWorldWithClass/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using myLibrary;
+
+
+//Declare class object
+var myObj = new myClass();
+
+Console.WriteLine("Hello");
+
+string input_1, input_2;
+double num_1, num_2, output;
+double[] inputs;
+
+Console.WriteLine("\nType any nuumber: ");
+input_1 = Console.ReadLine(); //string input from user
+num_1 = Double.Parse(input_1); //Converting it to double
+
+Console.WriteLine("\nType another nuumber: ");
+input_2 = Console.ReadLine(); //string input from user
+num_2 = Double.Parse(input_2); //Converting it to double
+
+inputs = new double[] { num_1, num_2 };
+output = myObj.sum(inputs);
+
+
+Console.WriteLine("\n The summation between " + num_1 + " and " + num_2 + " is " + output);
+Console.ReadLine();
+
