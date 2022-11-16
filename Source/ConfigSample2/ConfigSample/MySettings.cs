9912d376f31562547d98bdb48749c1024f2d5853
paweenp <paween.pongsomboon@gmail.com>
2022-11-16 10:58:40 +0100

    add samples for Nuget and onfiguration

---
+++ Source/ConfigSample2/ConfigSample/MySettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfigSample
+{
+    public class MySettings
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int age { get; set; }
+        public string country { get; set; }
+        public string tel { get; set; }
+        public string email { get; set; }
+
+    }
+}
