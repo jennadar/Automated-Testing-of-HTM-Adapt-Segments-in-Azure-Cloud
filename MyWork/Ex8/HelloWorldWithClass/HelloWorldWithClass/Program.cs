36a2909d7f5db7bc0125f16912814e6512a94ac0
Theertha <theertha1808@gmail.com>
2022-11-29 23:49:12 +0100

    EX_8_Interfaces_and_classes

---
+++ MyWork/Ex8/HelloWorldWithClass/HelloWorldWithClass/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+using myLibrary;
+using System.Drawing;
+using Triangle = myLibrary.Triangle;
+class Program
+{
+    static void Main(string[] arg)
+    {
+        int ch;
+        Console.WriteLine("1. Rectangle");
+        Console.WriteLine("2. Triangle");
+        Console.WriteLine("Enter your choice");
+        ch = Convert.ToInt32(Console.ReadLine());
+        if (ch == 1)
+        {
+            double w, h;
+            Rectangle r = new Rectangle();
+            Console.WriteLine("Enter Width: ");
+            w = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Height: ");
+            h = Convert.ToDouble(Console.ReadLine());
+
+        }
+        else if (ch == 2)
+        {
+            double w, h;
+            Triangle t = new Triangle();
+            Console.WriteLine("Enter Base: ");
+            w = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Height: ");
+            h = Convert.ToDouble(Console.ReadLine());
+        }
+        else
+        {
+            Console.WriteLine("Invalid Choice");
+        }
+    }
+
+}
---
+++ MyWork/Ex8/HelloWorldWithClass/myLibrary/Area.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myLibrary
+{
+      public interface Area
+    {
+        void Area(double w, double h);
+    }
+}
