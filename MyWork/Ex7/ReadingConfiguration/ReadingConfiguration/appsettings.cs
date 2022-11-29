0873d6b7473a0a2207365c53a5f2d21ca3dad414
Theertha <theertha1808@gmail.com>
2022-11-29 19:24:48 +0100

    EX_7_Reading Configuration,_Reading Files,_Reading file

---
+++ MyWork/Ex7/ReadingConfiguration/ReadingConfiguration/appsettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadingConfiguration
+{
+    internal class appsettings
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Country { get; set; }
+        public string Email { get; set; }
+    }
+}
