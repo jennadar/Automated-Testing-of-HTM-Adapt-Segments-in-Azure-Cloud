c74cee28498291dfe0eb4d714473fa5abbf65730
jennadar <jennynadar07@gmail.com>
2023-07-09 11:58:27 +0200

    Added account keys

--- Source/MyCloudProjectSample/MyCloudProject/Program.cs
+++ Source/MyCloudProjectSample/MyCloudProject/Program.cs
@@ -13,7 +13,7 @@ namespace MyCloudProject
         /// <summary>
         /// Your project ID from the last semester.
         /// </summary>
-        private static string projectName = "ML19/20-5.8";
+        private static string projectName = "ML 22/23-6";
 
         string test;
 
